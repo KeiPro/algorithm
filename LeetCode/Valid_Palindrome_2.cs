3b91467c4af1a894781e60572cd3f001f2f9a0ba
Kim youngwoo <keiprogramo@gmail.com>
2024-02-29 15:25:58 +0900

    LeetCode폴더 추가 및 Valid Palindrome2문제 커밋

---
+++ LeetCode/Valid_Palindrome_2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorhtim
+{
+    // 680. Valid Palindrome II
+    // https://leetcode.com/problems/valid-palindrome-ii/description/
+
+    class Class1
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+
+            //string s = "abcba";
+            //string s = "abccfba";
+            string s = "abcba";
+            Console.WriteLine(sol.ValidPalindrome(s));
+        }
+
+        public class Solution
+        {
+            public bool ValidPalindrome(string s)
+            {
+                return isValidPalindromeHelper(s, 0, s.Length-1, false);
+            }
+
+            private bool isValidPalindromeHelper(string s, int left, int right, bool isDeleted)
+            {
+                while (left < right)
+                {
+                    if (s[left] != s[right])
+                    {
+                        if (isDeleted)
+                            return false;
+
+                        return isValidPalindromeHelper(s, left + 1, right, true) || isValidPalindromeHelper(s, left, right - 1, true);
+                    }
+
+                    left++;
+                    right--;
+                }
+
+                return true;
+            }
+        }
+    }
+
+
+}
